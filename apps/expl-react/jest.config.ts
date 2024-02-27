dae0468c56790d776b6d516e67217626cff8bee7
Qian Zhiqiang <qianzq@touzhiwang.com>
2024-02-27 15:04:54 +0800

    新建app: npx nx g @nx/react:app expl-react --directory\=apps/expl-react

---
+++ apps/expl-react/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'expl-react',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/expl-react',
+};
