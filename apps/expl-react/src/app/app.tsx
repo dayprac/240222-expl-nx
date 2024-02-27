418ff84ece620198fe8e8a8309a6770f10d4ce62
Qian Zhiqiang <qianzq@touzhiwang.com>
2024-02-27 15:29:51 +0800

    尝试了ahooks里的updateEffectCount
    - 注释掉StrictMode

--- apps/expl-react/src/app/app.tsx
+++ apps/expl-react/src/app/app.tsx
@@ -3,9 +3,35 @@ import styles from './app.module.scss';
 
 import NxWelcome from './nx-welcome';
 
+import React, { useState, useEffect } from 'react';
+import { useUpdateEffect } from 'ahooks';
+
 export function App() {
+  const [count, setCount] = useState(0);
+  const [effectCount, setEffectCount] = useState(0);
+  const [updateEffectCount, setUpdateEffectCount] = useState(0);
+
+  useEffect(() => {
+    setEffectCount((c) => c + 1);
+  }, [count]);
+
+  useUpdateEffect(() => {
+    setUpdateEffectCount((c) => c + 1);
+    return () => {
+      // do something
+    };
+  }, [count]); // you can include deps array if necessary
   return (
     <div>
+      <div>
+        <p>effectCount: {effectCount}</p>
+        <p>updateEffectCount: {updateEffectCount}</p>
+        <p>
+          <button type="button" onClick={() => setCount((c) => c + 1)}>
+            reRender
+          </button>
+        </p>
+      </div>
       <NxWelcome title="expl-react" />
     </div>
   );
